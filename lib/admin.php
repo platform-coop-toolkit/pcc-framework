1332c8534db333d5691a021130b50dcf8c72ee4b
Ned Zimmerman <ned@bight.ca>
2019-11-15 13:08:04 -0700

    Don't relabel tags

--- lib/admin.php
+++ lib/admin.php
@@ -11,22 +11,3 @@ function enqueue_assets()
         PCC_FRAMEWORK_VERSION
     );
 }
-
-function relabel_tags()
-{
-    global $wp_taxonomies;
-    $labels = &$wp_taxonomies['post_tag']->labels;
-    $labels->name = __('Topics', 'pcc-framework');
-    $labels->menu_name = $labels->name;
-    $labels->singular_name = __('Topics', 'pcc-framework');
-    $labels->search_items = __('Search Topics', 'pcc-framework');
-    $labels->all_items = __('Topics', 'pcc-framework');
-    $labels->separate_items_with_commas = __('Separate Topics with commas', 'pcc-framework');
-    $labels->choose_from_most_used = __('Choose from the most used Topics', 'pcc-framework');
-    $labels->popular_items = __('Popular Topics', 'pcc-framework');
-    $labels->edit_item = __('Edit Topic', 'pcc-framework');
-    $labels->view_item = __('View Topic', 'pcc-framework');
-    $labels->update_item = __('Update Topic', 'pcc-framework');
-    $labels->add_new_item = __('Add New Topic', 'pcc-framework');
-    $labels->new_item_name = __('Your New Topics Name', 'pcc-framework');
-}
--- pcc-framework.php
+++ pcc-framework.php
@@ -104,7 +104,6 @@ if (is_admin()) {
     add_action('cmb2_admin_init', '\\PCCFramework\\Settings\\page');
     add_filter('attachment_fields_to_edit', '\\PCCFramework\\PostTypes\\Attachment\\data', 10, 2);
     add_action('edit_attachment', '\\PCCFramework\\PostTypes\\Attachment\\save');
-    add_action('init', '\\PCCFramework\\Admin\\relabel_tags');
 }
 
 require_once dirname(__FILE__) . '/lib/intervention.php';
